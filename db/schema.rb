13e583dba4b4f64384be7d8ed4f213f1362e13a9
g.tholbok@gmail.com <g.tholbok@gmail.com>
2017-04-07 09:26:43 +1000

    works on local

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20170406045849) do
-=======
 ActiveRecord::Schema.define(version: 20170406050338) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
